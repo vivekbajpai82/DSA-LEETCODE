ef489ba91a781940b6041445cf60b73ba87c5cff
Vivek Bajpai <146698553+vivekbajpai82@users.noreply.github.com>
2025-08-25 22:57:12 +0530

    Time: 33 ms (17.75%), Space: 31.2 MB (23.15%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        int n=mat.size();
+        int m=mat[0].size();
+        map<int, vector<int>> mp;
+        vector<int> res;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                mp[i+j].push_back(mat[i][j]);
+            }
+        }
+        bool flip=true;
+        for(auto &it : mp){
+            if(flip){
+                reverse(it.second.begin(),it.second.end());
+            }
+            for(int &num : it.second){
+                res.push_back(num);
+            }
+            flip=!flip;
+        }
+        return res;
+    }
+};
