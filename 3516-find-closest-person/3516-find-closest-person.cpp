eaf39ac553659223978ea94a96ca98e14324cf01
Vivek Bajpai <146698553+vivekbajpai82@users.noreply.github.com>
2025-09-04 20:45:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (21.23%) - LeetHub

---
+++ 3516-find-closest-person/3516-find-closest-person.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int a=abs(z-x);
+        int b=abs(z-y);
+        if(a==b) return 0;
+        if(a<b){
+            return 1;
+        } else{
+            return 2;
+        }
+    }
+};
